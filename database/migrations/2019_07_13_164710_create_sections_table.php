0d05c4e65145e65c0e8ad700159a12712867a998
Fran Navas <navasfran98@gmail.com>
2019-07-13 10:52:18 -0600

    new Model, migration, factory Section

---
+++ database/migrations/2019_07_13_164710_create_sections_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSectionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sections', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->unsignedBigInteger('course_id');
+            $table->timestamps();
+            $table->softDeletes();
+
+            //relationships
+            $table->foreign('course_id')->references('id')->on('courses');
+
+            //others
+            $table->charset = 'utf8';   
+            $table->collation = 'utf8_unicode_ci';
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sections');
+    }
+}
